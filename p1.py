923c607627266f906e8d9cedc22dc8abf7c331bd
Shanto <shantostatmbstu@gmail.com>
2024-02-16 17:20:30 +0600

    Added str to score and did some modification to functions

--- p1.py
+++ p1.py
@@ -7,13 +7,13 @@ if game == "no":
     quit()
 print("Before starting the game we need to ask you some questions") 
 #personal Questions
-q1=input("What is your name? ").strip().title()   
-print(q1)
+name=input("What is your name? ").strip().title()   
+print(name)
 q2=input("Where do you live in? ").strip().title()
 print(q2)
 q3= input("What do you do for living? ").strip().title()
 print(q3)
-print(f"Hello,{q1}.Let's play the game now..........")
+print(f"Hello,{name}.Let's play the game now..........")
 score=0
 Q1= input("Who is the current president of USA? ").lower()
 if Q1!="joe biden":
@@ -28,7 +28,7 @@ if Q2.lower()=="world health organization":
 else:
     print("Incorrect Answer") 
     score+=-8
-print(f"Your total score is {score}")
+print(f"{name},Your total score is {str(score)}")
 if score<=1:
     print("Very Bad")
 elif score>=2:
