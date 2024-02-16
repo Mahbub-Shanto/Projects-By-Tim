4a85732cfbe0ec1da44c4233a316b8349c8458da
Shanto <shantostatmbstu@gmail.com>
2024-02-16 12:12:06 +0600

    p1,py

--- p1.py
+++ p1.py
@@ -14,3 +14,24 @@ print(q2)
 q3= input("What do you do for living? ").strip().title()
 print(q3)
 print(f"Hello,{q1}.Let's play the game now..........")
+score=0
+Q1= input("Who is the current president of USA? ").lower()
+if Q1!="joe biden":
+    print("Incorrect Answer")
+else:
+    print("Correct Answer")
+    score+=1
+Q2=input("What is the full form of WHO ? ") 
+if Q2.lower()=="world health organization":
+    print("Correct Answer")
+    score+=5
+else:
+    print("Incorrect Answer") 
+    score+=-8
+print(f"Your total score is {score}")
+if score<=1:
+    print("Very Bad")
+elif score>=2:
+    print("Well done") 
+else:
+    print("Confusing Result")         
