7ad4d1f8b8f0d3b06e4b63c0bf16faa4db57d7a4
Shanto <shantostatmbstu@gmail.com>
2024-02-18 23:15:55 +0600

    for loops

--- CS50Week2/practice_of_lecture.py
+++ CS50Week2/practice_of_lecture.py
@@ -1,5 +1,12 @@
-#while loop type 2
-i=0
-while i<=10:
-    print("shanto")
-    i+=1
+#for loop
+for i in [0,1,2]:
+    print("meow")
+    
+#another way
+for i in range(100):
+    print("meow")    
+
+#another way because we are not using the variable i
+for _ in range(4):
+    print("mahbub") 
+    
